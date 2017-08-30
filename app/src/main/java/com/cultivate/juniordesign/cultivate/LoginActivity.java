c8dfc779bd73989f17379cf890ab639b8c10e59a
ehuskins3 <ethuskins@gatech.edu>
2017-08-30 16:31:21 -0400

    removed default

--- app/src/main/java/com/cultivate/juniordesign/cultivate/LoginActivity.java
+++ app/src/main/java/com/cultivate/juniordesign/cultivate/LoginActivity.java
@@ -52,13 +52,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+
 
 
     // UI references.
