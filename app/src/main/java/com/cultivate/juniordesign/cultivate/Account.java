8eb4afe9fafbb8c4a9930f0cc625b2b05ac3ca3d
ehuskins3 <ethuskins@gatech.edu>
2017-09-10 15:12:51 -0400

    fixed firebase access issues

--- app/src/main/java/com/cultivate/juniordesign/cultivate/Account.java
+++ app/src/main/java/com/cultivate/juniordesign/cultivate/Account.java
@@ -1,12 +1,15 @@
 package com.cultivate.juniordesign.cultivate;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by emilyhuskins on 9/6/17.
  */
 
-public class Account {
+public class Account implements Parcelable {
 
     int accountID;
     String firstName;
@@ -25,6 +28,86 @@ public class Account {
         email = memail;
     }
 
+    private Account(Parcel in) {
+        this(in.readString());
+    }
+
+    public Account() {
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(email);
+
+    }
+
+    public static final Parcelable.Creator<Account> CREATOR
+            = new Parcelable.Creator<Account>() {
+        @Override
+        public Account createFromParcel(Parcel in) {
+            return new Account(in);
+        }
+
+        @Override
+        public Account[] newArray(int size) {
+            return new Account[size];
+        }
+    };
+
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Integer> getMemberGroups() {
+        return memberGroups;
+    }
+
+    public void setMemberGroups(List<Integer> memberGroups) {
+        this.memberGroups = memberGroups;
+    }
+
+    public List<Integer> getManageGroups() {
+        return manageGroups;
+    }
+
+    public void setManageGroups(List<Integer> manageGroups) {
+        this.manageGroups = manageGroups;
+    }
 
 
 
