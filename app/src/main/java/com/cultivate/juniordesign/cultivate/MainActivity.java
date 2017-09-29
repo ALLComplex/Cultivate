1ce48ad107459dec97d29bc25c53da1217624af4
cathylefebvre <cathy.e.lefebvre@gmail.com>
2017-09-29 11:44:05 -0400

    Adds Hamburger Bar
    Adds the hamburger bar with minimal functionality at every view.

--- app/src/main/java/com/cultivate/juniordesign/cultivate/MainActivity.java
+++ app/src/main/java/com/cultivate/juniordesign/cultivate/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cultivate.juniordesign.cultivate;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
 import android.content.Intent;
@@ -16,7 +20,6 @@ import static android.R.attr.duration;
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,24 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         Snackbar email = Snackbar.make((findViewById(R.id.home_layout)),"e-mail: " + user.getEmail(), 10000);
         email.show();
+    }
+
+    public void openHamburgerBar(View v) {
+        // get a reference to the already created main layout
+        ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.home_layout);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.hamburgerbar_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupWindow.showAtLocation(mainLayout, Gravity.TOP|Gravity.LEFT, 0, 0);
     }
 
     public void goToStub(View v){
@@ -49,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
     public void goToGroup(View v) {
         Intent group = new Intent(this, GroupActivity.class);
         startActivity(group);
+    }
+
+    public void notImplemented(View v) {
+        Toast.makeText(MainActivity.this, "This feature is not yet implemented",Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/cultivate/juniordesign/cultivate/ProfileActivity.java
+++ app/src/main/java/com/cultivate/juniordesign/cultivate/ProfileActivity.java
@@ -2,9 +2,15 @@ package com.cultivate.juniordesign.cultivate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -27,5 +33,47 @@ public class ProfileActivity extends AppCompatActivity {
     public void goToHome(View v) {
         Intent event = new Intent(this, MainActivity.class);
         startActivity(event);
+    }
+
+    public void openHamburgerBar(View v) {
+        // get a reference to the already created main layout
+        ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.profile_layout);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.hamburgerbar_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupWindow.showAtLocation(mainLayout, Gravity.TOP | Gravity.LEFT, 0, 0);
+    }
+
+    public void goToStub(View v) {
+        Intent stub = new Intent(this, StubActivity.class);
+        startActivity(stub);
+    }
+
+    public void goToEvent(View v) {
+        Intent event = new Intent(this, EventActivity.class);
+        startActivity(event);
+    }
+
+    public void goToProfile(View v) {
+        Intent profile = new Intent(this, ProfileActivity.class);
+        startActivity(profile);
+    }
+
+    public void goToGroup(View v) {
+        Intent group = new Intent(this, GroupActivity.class);
+        startActivity(group);
+    }
+
+    public void notImplemented(View v) {
+        Toast.makeText(ProfileActivity.this, "This feature is not yet implemented", Toast.LENGTH_SHORT).show();
     }
 }
