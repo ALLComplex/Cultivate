90084be58a90b5d4240e223aad1a942cb2368105
cathylefebvre <cathy.e.lefebvre@gmail.com>
2017-11-02 18:03:53 -0400

    Creation of Event Time UI
    The event creation options for time now shows up . It isn't implemented from the Database side so isnt actually functional. Also creates Utility class for parsing time information between database and user view

--- app/src/main/java/com/cultivate/juniordesign/cultivate/ActivityClass/CreateEventActivity.java
+++ app/src/main/java/com/cultivate/juniordesign/cultivate/ActivityClass/CreateEventActivity.java
@@ -1,10 +1,17 @@
 package com.cultivate.juniordesign.cultivate.ActivityClass;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.constraint.ConstraintLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cultivate.juniordesign.cultivate.Account;
@@ -31,8 +38,21 @@ public class CreateEventActivity extends HamburgerActivity {
         curGroup = getIntent().getParcelableExtra("curGroup");
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextLocation = (EditText) findViewById(R.id.editTextLocation);
+        setSpinnerAdapter(R.id.months, R.array.months);
+        setSpinnerAdapter(R.id.days, R.array.days);
+        setSpinnerAdapter(R.id.years, R.array.years);
+        setSpinnerAdapter(R.id.startHour, R.array.hours);
+        setSpinnerAdapter(R.id.endHour, R.array.hours);
+        setSpinnerAdapter(R.id.endMinute, R.array.minutes);
+        setSpinnerAdapter(R.id.startMinute, R.array.minutes);
+        setSpinnerAdapter(R.id.startAM, R.array.amPm);
+        setSpinnerAdapter(R.id.endAm, R.array.amPm);
     }
 
+    /**
+     *
+     * @param v
+     */
     public void createEvent(View v) {
         //TODO: check if group name exists.
         String name = editTextName.getText().toString();
@@ -46,11 +66,60 @@ public class CreateEventActivity extends HamburgerActivity {
         goToEventActivity(v, event);
     }
 
+    /**
+     *
+     * @param v
+     * @param e
+     */
     public void goToEventActivity(View v, Event e) {
         Intent event = new Intent(this, EventActivity.class);
         event.putExtra("curUser", user);
         event.putExtra("curEvent", e);
         startActivity(event);
+    }
+
+    /**
+     *
+     * @param v
+     */
+    public void allDayBoxClicked(View v){
+        if (((CheckBox) v).isChecked()) {
+            setTimeGone();
+        } else {
+            setTimeVisible();
+        }
+    }
+
+    /**
+     *
+     */
+    public void setTimeGone(){
+        ((TextView) findViewById(R.id.textStart)).setVisibility(View.GONE);
+        ((TextView) findViewById(R.id.textEnd)).setVisibility(View.GONE);
+        ((TableLayout)findViewById(R.id.tableEndTime)).setVisibility(View.GONE);
+        ((TableLayout)findViewById(R.id.tableStartTime)).setVisibility(View.GONE);
+    }
+
+    /**
+     *
+     */
+    public void setTimeVisible(){
+        ((TextView) findViewById(R.id.textStart)).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.textEnd)).setVisibility(View.VISIBLE);
+        ((TableLayout)findViewById(R.id.tableEndTime)).setVisibility(View.VISIBLE);
+        ((TableLayout)findViewById(R.id.tableStartTime)).setVisibility(View.VISIBLE);
+    }
+
+    /**
+     *
+     * @param spinnerView
+     * @param resourceView
+     */
+    private void setSpinnerAdapter(int spinnerView, int resourceView) {
+        Spinner spinner = (Spinner) findViewById(spinnerView);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(resourceView));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
 }
